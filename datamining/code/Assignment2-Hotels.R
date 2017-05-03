3dd8ac145051f38ea8fe774b7fc0e7699935483d
jelfiedia <jelle.schutte@student.uva.com>
2017-05-03 11:15:41 +0200

    Fourth commitment

---
+++ datamining/code/Assignment2-Hotels.R
@@ -0,0 +1,28 @@
+#Set working directory
+setwd("D:/Master/Data Mining/Ass2")
+
+#Read training and test data from the .csv files in the working directory
+# train.hotels <- read.csv("Data/training_set_VU_DM_2014.csv", header = T, stringsAsFactors = F)
+# test.hotels <- read.csv("Data/test_set_VU_DM_2014.csv", header = T, stringsAsFactors = F)
+# save.image('traintest.Rdata')
+
+#read data from the environment, as it is a large dataset
+load("traintest.RData")
+
+######################################################
+#                 Data Understanding                 #
+######################################################
+
+library(doMC)
+registerDoMC(4)
+
+#first create a summary of the training dataset
+summary(train.hotels)
+
+#Check for missing values
+library(VIM)
+library(Amelia)
+missmap(train.hotels)
+
+
+barMiss(train.hotels)
